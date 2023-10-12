2e71190ddee201fd74341f67b38783774692ce19
Ibrhaim <54852400+mohdibrahim1995@users.noreply.github.com>
2023-10-12 10:21:37 +0000

    Scops_block_Global_scopes

---
+++ .devcontainer/03_basics/02_scops.js
@@ -0,0 +1,12 @@
+let a=400
+if(true){
+    let a=20;//block scope 
+    const b=20;     //block scope 
+var c=300; //globalscope not a good pratice to use this
+
+}
+
+console.log(a);
+// console.log(b);
+console.log(c);
+
