5891031d623cde46461859f334446b8cdaaf4699
Ibrhaim <54852400+mohdibrahim1995@users.noreply.github.com>
2023-10-12 11:29:45 +0000

    function_and_Variable

--- .devcontainer/03_basics/02_scops.js
+++ .devcontainer/03_basics/02_scops.js
@@ -6,7 +6,34 @@ var c=300; //globalscope not a good pratice to use this
 
 }
 
-console.log(a);
-// console.log(b);
-console.log(c);
+// console.log(a);
+// // console.log(b);
+// console.log(c);
+
+// function one(){
+//     const UserName="Mohd Ibrahim" //Global Scope
+//     function two(){
+//         const website="mohammed1995@youtube.com"  //block or functional scope
+//         console.log(UserName);
+//     }
+//     console.log(website);
+//     two();
+// }
+
+// one();
+
+
+
+// basic Function 
+function addOne(num){ 
+    return num +1;
+}
+console.log(addOne(5))
+
+// variable hosting
+const varibalehosting= function addtwo(num){  /// variable which holds a value of function sometimes it is also called as hosting 
+    return num+2;
+}
+console.log(varibalehosting(6))
+
 
